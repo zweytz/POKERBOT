318c927470e41ad3bdae7bca17834acb0f92abe4
JCBWLL <jacob.wall7@gmail.com>
2022-04-05 18:44:58 +1000

    updated play function

---
+++ jak.py
@@ -0,0 +1,8 @@
+def play(player, flop, current_bet, players_still_in):
+
+    pass
+
+# return formatting:
+# for fold, return 'fold'
+# for raise, return 'raise {amount}' e.g 'raise 10'
+# for call, return 'call'
